466e240e5049af97f9c2e45d7c5ae564fe5ba1b1
Remi Laforgue <relaforg@z3r4p1.42lyon.fr>
2026-02-27 16:35:38 +0100

    Refacto constrained

--- src/__main__.py
+++ src/__main__.py
@@ -1,278 +1,79 @@
-from src.llm_sdk import Small_LLM_Model
-from math import log, exp
-from typing import List
+from .constrained_decoding import ConstrainedDecoding
+from pprint import pprint
+import json
 
 
-llm = Small_LLM_Model()
-out: List[int] = []
+# func_dict = {
+#     "fn_add_numbers": {
+#         "description": "Add two numbers together and return their sum.",
+#         "parameters": {
+#             "a": {
+#                 "type": "number"
+#             },
+#             "b": {
+#                 "type": "number"
+#             }
+#         },
+#         "returns": {
+#             "type": "number"
+#         }
+#     },
+#     "fn_greet": {
+#         "description": "Generate a greeting message for a person by name.",
+#         "parameters": {
+#             "name": {
+#                 "type": "string"
+#             }
+#         },
+#         "returns": {
+#             "type": "string"
+#         }
+#     },
+#     "fn_reverse_string": {
+#         "description": "Reverse a string and return the reversed result.",
+#         "parameters": {
+#             "s": {
+#                 "type": "string"
+#             }
+#         },
+#         "returns": {
+#             "type": "string"
+#         }
+#     },
+#     "fn_get_square_root": {
+#         "description": "Calculate the square root of a number.",
+#         "parameters": {
+#             "a": {
+#                 "type": "number"
+#             }
+#         },
+#         "returns": {
+#             "type": "number"
+#         }
+#     },
+#     "fn_substitute_string_with_regex": {
+#         "description": "Replace all occurrences matching \
+# a regex pattern in a string.",
+#         "parameters": {
+#             "source_string": {
+#                 "type": "string"
+#             },
+#             "regex": {
+#                 "type": "string"
+#             },
+#             "replacement": {
+#                 "type": "string"
+#             }
+#         },
+#         "returns": {
+#             "type": "string"
+#         }
+#     }
+# }
 
-
-def choose_constant_token(logits, token):
-    for i in range(len(logits)):
-        if (i != token):
-            logits[i] = float("-inf")
-
-
-def choose_constrained_token(logits, allowed) -> int:
-    max_t = float("-inf")
-    nxt = -1
-    for t in allowed:
-        if (logits[t] > max_t or nxt == -1):
-            nxt = t
-            max_t = logits[t]
-    return (nxt)
-
-
-def get_max_logits_index(logits):
-    max_i = 0
-    for i, v in enumerate(logits):
-        if logits[max_i] < v:
-            max_i = i
-    return (max_i)
-
-
-def logsumexp(xs):
-    m = max(xs)
-    return m + log(sum(exp(x - m) for x in xs))
-
-
-def compute_avg_logprob(prob_list):
-    if (len(prob_list) == 0):
-        return (0)
-    sum = 0
-    for i in prob_list:
-        sum += i
-    return (sum / len(prob_list))
-
-
-def choose_func(func_dict, tokens, out):
-    best_name = ""
-    best_prob = float("-inf")
-    for name, value in func_dict.items():
-        logprob_list = []
-        name_tokens = llm.encode(name)[0].tolist()
-        tmp_tokens = list(tokens)
-        for t in range(len(name_tokens)):
-            logits = llm.get_logits_from_input_ids(tmp_tokens)
-            logprob_list.append(
-                logits[name_tokens[t]] - logsumexp(logits))
-            tmp_tokens.append(name_tokens[t])
-        avg_prob = compute_avg_logprob(logprob_list)
-        if (avg_prob > best_prob):
-            best_prob = avg_prob
-            best_name = name
-    out += llm.encode(best_name)[0].tolist()
-    return (best_name)
-
-
-def get_number_allowed():
-    nbrs = set()
-    for i in "0123456789.-":
-        nbrs.add(llm.encode(i)[0].tolist()[0])
-    return (nbrs)
-
-
-NUMBER_ALLOWED = get_number_allowed()
-
-
-def get_number_param(context, prompt_token):
-    allowed = NUMBER_ALLOWED & set(prompt_token)
-    param_tokens = []
-    while (True):
-        logits = llm.get_logits_from_input_ids(context)
-        max_t = float("-inf")
-        nxt = -1
-        for t in allowed:
-            if (logits[t] > max_t or nxt == -1):
-                nxt = t
-                max_t = logits[t]
-        if (llm.decode(param_tokens + [nxt]) not in llm.decode(prompt_token)):
-            break
-        out.append(nxt)
-        context.append(nxt)
-        param_tokens.append(nxt)
-
-
-def get_string_param(context, prompt_token):
-    param_tokens = []
-    quote_token = llm.encode("\"")[0].tolist()[0]
-    context.append(quote_token)
-    out.append(quote_token)
-    allowed = set(prompt_token)
-    while (True):
-        logits = llm.get_logits_from_input_ids(context)
-        max_t = float("-inf")
-        nxt = -1
-        for t in allowed:
-            if (logits[t] > max_t or nxt == -1):
-                nxt = t
-                max_t = logits[t]
-        if (llm.decode(param_tokens + [nxt]) not in llm.decode(prompt_token)):
-            break
-        out.append(nxt)
-        context.append(nxt)
-        param_tokens.append(nxt)
-    context.append(quote_token)
-    out.append(quote_token)
-
-
-def get_bool_allowed():
-    allowed = set()
-    for i in ["true", "false"]:
-        allowed.add(llm.encode(i)[0].tolist()[0])
-    return (allowed)
-
-
-BOOL_ALLOWED = get_bool_allowed()
-
-
-def get_bool_param(context):
-    logits = llm.get_logits_from_input_ids(context)
-    max_t = float("-inf")
-    nxt = -1
-    for t in BOOL_ALLOWED:
-        if (logits[t] > max_t or nxt == -1):
-            nxt = t
-            max_t = logits[t]
-    out.append(nxt)
-    context.append(nxt)
-
-
-def get_param(context, param_type, prompt_token):
-    match param_type["type"]:
-        case "number":
-            return get_number_param(context, prompt_token)
-        case "bool" | "boolean":
-            return get_bool_param(context)
-        case "string" | _:
-            return get_string_param(context, prompt_token)
-
-
-schema = """
-{
-\t"prompt": "<tool_call>",
-\t"name": "<tool_call>",
-\t"parameters": {<tool_call>}
-}
-<|endoftext|>
-"""
-
-func_dict = {
-    "fn_add_numbers": {
-        "description": "Add two numbers together and return their sum.",
-        "parameters": {
-            "a": {
-                "type": "number"
-            },
-            "b": {
-                "type": "number"
-            }
-        },
-        "returns": {
-            "type": "number"
-        }
-    },
-    "fn_greet": {
-        "description": "Generate a greeting message for a person by name.",
-        "parameters": {
-            "name": {
-                "type": "string"
-            }
-        },
-        "returns": {
-            "type": "string"
-        }
-    },
-    "fn_reverse_string": {
-        "description": "Reverse a string and return the reversed result.",
-        "parameters": {
-            "s": {
-                "type": "string"
-            }
-        },
-        "returns": {
-            "type": "string"
-        }
-    },
-    "fn_get_square_root": {
-        "description": "Calculate the square root of a number.",
-        "parameters": {
-            "a": {
-                "type": "number"
-            }
-        },
-        "returns": {
-            "type": "number"
-        }
-    },
-    "fn_substitute_string_with_regex": {
-        "description": "Replace all occurrences matching \
-a regex pattern in a string.",
-        "parameters": {
-            "source_string": {
-                "type": "string"
-            },
-            "regex": {
-                "type": "string"
-            },
-            "replacement": {
-                "type": "string"
-            }
-        },
-        "returns": {
-            "type": "string"
-        }
-    }
-}
+with open("data/input/functions_definition.json", "r") as file:
+    data = json.load(file)
 
 prompt = "What is the sum of 40 and 2?"
-context = llm.encode(prompt)[0].tolist()
-tokens_base = list(context)
-func_string = "functions:\n"
-for name, value in func_dict.items():
-    func_string += f"- {name} ({value['parameters']})\n"
-context += llm.encode(func_string)[0].tolist()
-test = llm.encode(schema)
-tokens = test[0].tolist()
-count = 0
-for i in tokens:
-    logits = llm.get_logits_from_input_ids(context)
-    if (i != 151657):
-        nxt = choose_constrained_token(logits, {i})
-        out.append(nxt)
-        context.append(nxt)
-    elif (count == 0):
-        for j in tokens_base:
-            logits = llm.get_logits_from_input_ids(context)
-            nxt = choose_constrained_token(logits, {j})
-            out.append(nxt)
-            context.append(nxt)
-        count += 1
-    elif (count == 1):
-        func_name = choose_func(func_dict, context, out)
-        context += llm.encode(func_name + " " +
-                              f"\n{str(func_dict[func_name])}")[0].tolist()
-        count += 1
-    elif (count == 2):
-        COMMA_SPACE = llm.encode(", ")[0].tolist()
-        for c, param in enumerate(func_dict[func_name]["parameters"].items()):
-            name, param_type = param
-            parameters = f'"{name}": '
-            param_tokens = llm.encode(parameters)[0].tolist()
-            for j in param_tokens:
-                logits = llm.get_logits_from_input_ids(context)
-                nxt = choose_constrained_token(logits, {j})
-                out.append(nxt)
-                context.append(nxt)
-            get_param(context, param_type, tokens_base)
-            if (c < len(func_dict[func_name]["parameters"]) - 1):
-                param_tokens = COMMA_SPACE
-                for j in param_tokens:
-                    logits = llm.get_logits_from_input_ids(context)
-                    nxt = choose_constrained_token(logits, {j})
-                    out.append(nxt)
-                    context.append(nxt)
-        count += 1
-    if (i == 151643):
-        break
-print(llm.decode(out))
+test = ConstrainedDecoding(data)
+print(test.run(prompt))
---
+++ src/constrained_decoding.py
@@ -0,0 +1,208 @@
+from llm_sdk import Small_LLM_Model
+from torch import Tensor
+from typing import Set
+from math import exp, log
+
+
+class ConstrainedDecoding:
+    def __init__(self, func_dict):
+        self.llm = Small_LLM_Model()
+        self.func_dict = func_dict
+        self.out = []
+        self.context = []
+        self.prompt_tokens = []
+        self.NUMBER_ALLOWED = self._get_number_allowed()
+        self.QUOTE_TOKEN = self.llm.encode("\"")[0].tolist()[0]
+        self.BOOL_ALLOWED = self._get_bool_allowed()
+        self.SCHEMA = """
+{
+\t"prompt": "<tool_call>",
+\t"name": "<tool_call>",
+\t"parameters": {<tool_call>}
+}
+<|endoftext|>
+        """
+        self.SCHEMA_TOKENS = self.llm.encode(self.SCHEMA)[0].tolist()
+        self.TOOL_CALL_TOKEN = 151657
+        self.EOT_TOKEN = 151643
+
+    def _get_bool_allowed(self):
+        allowed = set()
+        for i in ["true", "false"]:
+            allowed.add(self.llm.encode(i)[0].tolist()[0])
+        return (allowed)
+
+    def _get_number_allowed(self):
+        nbrs = set()
+        for i in "0123456789.-":
+            nbrs.add(self.llm.encode(i)[0].tolist()[0])
+        return (nbrs)
+
+    def _choose_constrained_token(self, logits: Tensor, allowed: Set) -> int:
+        max_t = float("-inf")
+        nxt = -1
+        for t in allowed:
+            if (logits[t] > max_t or nxt == -1):
+                nxt = t
+                max_t = logits[t]
+        return (nxt)
+
+    def _logsumexp(self, xs):
+        m = max(xs)
+        return (m + log(sum(exp(x - m) for x in xs)))
+
+    def _compute_avg_logprob(self, prob_list):
+        if (len(prob_list) == 0):
+            return (0)
+        sum = 0
+        for i in prob_list:
+            sum += i
+        return (sum / len(prob_list))
+
+    def _choose_func(self):
+        best_name = ""
+        best_prob = float("-inf")
+        # for name, value in self.func_dict.items():
+        for name in [f["name"] for f in self.func_dict]:
+            logprob_list = []
+            name_tokens = self.llm.encode(name)[0].tolist()
+            tmp_tokens = list(self.context)
+            for t in range(len(name_tokens)):
+                logits = self.llm.get_logits_from_input_ids(tmp_tokens)
+                logprob_list.append(
+                    logits[name_tokens[t]] - self._logsumexp(logits))
+                tmp_tokens.append(name_tokens[t])
+            avg_prob = self._compute_avg_logprob(logprob_list)
+            if (avg_prob > best_prob):
+                best_prob = avg_prob
+                best_name = name
+        self.out += self.llm.encode(best_name)[0].tolist()
+        return (best_name)
+
+    def _is_subsequence(self, sub: list[int], seq: list[int]) -> bool:
+        n, m = len(sub), len(seq)
+        if n == 0:
+            return (True)
+        for i in range(m - n + 1):
+            if seq[i:i+n] == sub:
+                return (True)
+        return (False)
+
+    def _get_number_param(self):
+        allowed = self.NUMBER_ALLOWED & set(self.prompt_tokens)
+        param_tokens = []
+        while (True):
+            logits = self.llm.get_logits_from_input_ids(self.context)
+            max_t = float("-inf")
+            nxt = -1
+            for t in allowed:
+                if (logits[t] > max_t or nxt == -1):
+                    nxt = t
+                    max_t = logits[t]
+            if not self._is_subsequence(param_tokens
+                                        + [nxt], self.prompt_tokens):
+                break
+            self.out.append(nxt)
+            self.context.append(nxt)
+            param_tokens.append(nxt)
+
+    def _get_string_param(self):
+        param_tokens = []
+        self.context.append(self.QUOTE_TOKEN)
+        self.out.append(self.QUOTE_TOKEN)
+        allowed = set(self.prompt_tokens)
+        while (True):
+            logits = self.llm.get_logits_from_input_ids(self.context)
+            max_t = float("-inf")
+            nxt = -1
+            for t in allowed:
+                if (logits[t] > max_t or nxt == -1):
+                    nxt = t
+                    max_t = logits[t]
+            if not self._is_subsequence(param_tokens
+                                        + [nxt], self.prompt_tokens):
+                break
+            self.out.append(nxt)
+            self.context.append(nxt)
+            param_tokens.append(nxt)
+        self.context.append(self.QUOTE_TOKEN)
+        self.out.append(self.QUOTE_TOKEN)
+
+    def _get_bool_param(self):
+        logits = self.llm.get_logits_from_input_ids(self.context)
+        max_t = float("-inf")
+        nxt = -1
+        for t in self.BOOL_ALLOWED:
+            if (logits[t] > max_t or nxt == -1):
+                nxt = t
+                max_t = logits[t]
+        self.out.append(nxt)
+        self.context.append(nxt)
+
+    def _get_param(self, param_type):
+        match param_type["type"]:
+            case "number":
+                return self._get_number_param()
+            case "bool" | "boolean":
+                return self._get_bool_param()
+            case "string" | _:
+                return self._get_string_param()
+
+    def run(self, prompt: str) -> str:
+        self.context = self.llm.encode(prompt)[0].tolist()
+        self.out = []
+        self.prompt_tokens = list(self.context)
+        func_string = "functions:\n"
+        # for name, value in self.func_dict.items():
+        for f in self.func_dict:
+            func_string += f"- {f['name']} ({f['parameters']})\n"
+        self.context += self.llm.encode(func_string)[0].tolist()
+        count = 0
+        for i in self.SCHEMA_TOKENS:
+            logits = self.llm.get_logits_from_input_ids(self.context)
+            if (i != self.TOOL_CALL_TOKEN):
+                nxt = self._choose_constrained_token(logits, {i})
+                self.out.append(nxt)
+                self.context.append(nxt)
+            elif (count == 0):
+                for j in self.prompt_tokens:
+                    logits = self.llm.get_logits_from_input_ids(self.context)
+                    nxt = self._choose_constrained_token(logits, {j})
+                    self.out.append(nxt)
+                    self.context.append(nxt)
+                count += 1
+            elif (count == 1):
+                func_name = self._choose_func()
+                func_context = [
+                    f for f in self.func_dict if f["name"] == func_name][0]
+                self.context += self.llm.encode(func_name + " " +
+                                                f"\n{str(func_context)}")[0].tolist()
+                count += 1
+            elif (count == 2):
+                COMMA_SPACE = self.llm.encode(", ")[0].tolist()
+                func_context = [
+                    f for f in self.func_dict if f["name"] == func_name][0]
+                for c, param in enumerate(func_context["parameters"].items()):
+                    name, param_type = param
+                    parameters = f'"{name}": '
+                    param_tokens = self.llm.encode(parameters)[0].tolist()
+                    for j in param_tokens:
+                        logits = self.llm.get_logits_from_input_ids(
+                            self.context)
+                        nxt = self._choose_constrained_token(logits, {j})
+                        self.out.append(nxt)
+                        self.context.append(nxt)
+                    self._get_param(param_type)
+                    if (c < len(func_context["parameters"]) - 1):
+                        param_tokens = COMMA_SPACE
+                        for j in param_tokens:
+                            logits = self.llm.get_logits_from_input_ids(
+                                self.context)
+                            nxt = self._choose_constrained_token(logits, {j})
+                            self.out.append(nxt)
+                            self.context.append(nxt)
+                count += 1
+            if (i == self.EOT_TOKEN):
+                break
+        result = self.llm.decode(self.out)
+        return (result)
